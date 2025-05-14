5d7139e0eda1cf068b276596ab7fefde8afdac4c
dhvl2004 <duonghoangvulam@gmail.com>
2025-05-14 12:52:54 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './styles/global.css';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles/global.css";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/dhvl-portfolio">
       <div className="App">
         <Navbar />
         <main>
